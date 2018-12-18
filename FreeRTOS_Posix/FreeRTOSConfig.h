87068433f2f27a9951a122b9979dbbb82adf75fe
coon <coon@mailbox.org>
2018-12-18 14:41:05 +0100

    build fix for v8.0.0

--- FreeRTOS_Posix/FreeRTOSConfig.h
+++ FreeRTOS_Posix/FreeRTOSConfig.h
@@ -65,6 +65,7 @@
  *----------------------------------------------------------*/
 
 #define configUSE_STATS_FORMATTING_FUNCTIONS 1 // required since version 7.5.0. TODO: refactor and remove this setting
+#define configENABLE_BACKWARD_COMPATIBILITY 1 // required since version 8.0.0. TODO: refactor and remove this setting
 #define configUSE_PREEMPTION			1
 #define configUSE_IDLE_HOOK				1
 #define configUSE_TICK_HOOK				0
